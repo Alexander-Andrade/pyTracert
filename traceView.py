1e3f51740c76525363ad3f2dcae43a97bc7bb715
Alexander-Andrade <veimar.94@gmail.com>
2016-02-28 22:55:53 +0300

    final

--- traceView.py
+++ traceView.py
@@ -14,27 +14,33 @@ class TraceView(ttk.Frame):
         self.dest_text = ttk.Entry(self.dest_frame)
         self.dest_text.focus_set()
         self.dest_button = ttk.Button(self.dest_frame, text='trace', command=self.trace)
-        self.dest_label.pack(side='right', fill=BOTH, expand=YES)
-        self.dest_text.pack(side='right', fill=BOTH, expand=YES)
-        self.dest_button.pack(side='left', fill=BOTH, expand=YES)
-        self.dest_frame.pack(side='top', fill=BOTH, expand=YES)
+        self.bind('<Return>', self.trace)
+        self.dest_label.pack(side='right', fill=X, expand=YES)
+        self.dest_text.pack(side='right', fill=X, expand=YES)
+        self.dest_button.pack(side='left', fill=X, expand=YES)
+        self.dest_frame.pack(side='top', fill=X, expand=NO)
 
         self.tree = ttk.Treeview(self, selectmode="extended", columns=('ttl', 'domain_name', 'ip_v4', 'rtt'))
         # hide pseudo column
         self.tree.column('#0', stretch=NO, width=0)
-        self.tree.column('ttl', stretch=YES)
+        self.tree.column('ttl', stretch=YES, width=64)
         self.tree.heading('ttl', text='hop(ttl)')
-        self.tree.column('domain_name', stretch=YES)
+        self.tree.column('domain_name', stretch=YES, width=300)
         self.tree.heading('domain_name', text='domain name')
-        self.tree.column('ip_v4', stretch=YES)
+        self.tree.column('ip_v4', stretch=YES, width=128)
         self.tree.heading('ip_v4', text='IPv4')
-        self.tree.column('rtt', stretch=YES)
+        self.tree.column('rtt', stretch=YES, width=256)
         self.tree.heading('rtt', text='RTT')
         self.tree.pack(fill=BOTH, expand=YES)
 
     def clearView(self):
         for child in self.tree.get_children():
             self.tree.delete(child)
+
+    @staticmethod
+    def timeStamp(time):
+        return str(round(time*1000, 5))+'ms'
+
     def trace(self):
         self.clearView()
         if self.dest_text.get() != '':
@@ -42,7 +48,10 @@ class TraceView(ttk.Frame):
             for i in range(1, tracert.max_hops):
                 ping_res = tracert.ping(i)
                 if ping_res is not None:
-                    self.tree.insert('', 'end', values=(ping_res[0], ping_res[1], ping_res[2], str(round(ping_res[3][0]*1000, 5))+'ms'))
+                    self.tree.insert('', 'end', values=(ping_res[0], ping_res[1], ping_res[2],
+                                                        TraceView.timeStamp(ping_res[3][0]) + ' ' +
+                                                        TraceView.timeStamp(ping_res[3][1]) + ' ' +
+                                                        TraceView.timeStamp(ping_res[3][2])))
                     self.tree.update()
                     if ping_res[4]:
                         break
