cf3d366de8d8d18a2dbc4b374edfd3b24719dd3f
Alexander-Andrade <veimar.94@gmail.com>
2016-02-28 19:29:04 +0300

    traceroute+

--- pyTracert.py
+++ pyTracert.py
@@ -3,8 +3,13 @@ import struct
 import sys
 import time
 import os
+import math
 from ip_header import*
 from icmp_packet import*
+
+
+class DestinationReached(Exception):
+    pass
 
 
 class Tracert:
@@ -29,28 +34,42 @@ class Tracert:
     def ping(self, ttl):
         # send ICMP Echo-Request and catch ICMP Echo-Reply or ICMP Destination Unreachable
         time_stamps = []
+        fl_dest_reached = False
         ip_header = IP_Header(self.proc_id, ttl, IPPROTO_ICMP, self.host_ip, self.target_ip)
         for port in self.unused_ports:
             icmp_pack = ICMP_Packet(ICMP_Packet.ECHO_REQ_TYPE, ICMP_Packet.ECHO_REQ_CODE, self.proc_id, self.icmp_seq_num)
             start_time = time.time()
             self.send_sock.sendto(ip_header.pack() + icmp_pack.pack(), (self.target_ip, port))
-            icmp_reply, host_addr = self.catch_sock.recvfrom(1024)
-            time_stamps.append(time.time() - start_time)
-            repl_ip_header = IP_Header()
-            repl_icmp_pack = ICMP_Packet()
-            repl_ip_header.unpack(icmp_reply)
-            repl_icmp_pack.unpack(icmp_reply)
-            self.icmp_seq_num += 1
+            try:
+                icmp_reply, host_addr = self.catch_sock.recvfrom(1024)
+                time_stamps.append(time.time() - start_time)
+                repl_ip_header = IP_Header()
+                repl_icmp_pack = ICMP_Packet()
+                repl_ip_header.unpack(icmp_reply)
+                repl_icmp_pack.unpack(icmp_reply)
+                # test on ICMP Echo-Reply = destination reached
+                if repl_icmp_pack.type == ICMP_Packet.ECHO_REPLY_TYPE and repl_icmp_pack.code == ICMP_Packet.ECHO_REPLY_CODE:
+                    fl_dest_reached = True
+                self.icmp_seq_num += 1
+            except OSError as e:
+                # infinite expectation
+                return None
         # ttl, domain name, IPv4, [time1..time3]
-        return ttl, getfqdn(repl_ip_header.src_addr), repl_ip_header.src_addr, time_stamps
+        return ttl, getfqdn(repl_ip_header.src_addr), repl_ip_header.src_addr, time_stamps, fl_dest_reached
 
 
 if __name__ == '__main__':
-    #tracert = Tracert('www.google.com')
-    #tracert.ping(1)
-    print(gethostbyname('www.google.com'))
-    print(gethostbyaddr(gethostbyname('www.google.com')))
-    print(getfqdn('www.google.com'))
+    tracert = Tracert('docs.python.org')
+    print(gethostbyname('docs.python.org'))
+    fl_reached = False
+    for i in range(1, 255):
+        ping_results = tracert.ping(i)
+        print(ping_results)
+        if ping_results is not None:
+            fl_reached = ping_results[4]
+            if fl_reached:
+                break
+
 
 
 
