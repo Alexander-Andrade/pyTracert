d57206e75c98779f7ef85b3cae07e6d380a10ae6
Alexander-Andrade <veimar.94@gmail.com>
2016-02-28 21:01:38 +0300

    traceview

--- traceView.py
+++ traceView.py
@@ -8,8 +8,16 @@ class TraceView(ttk.Frame):
     def __init__(self, master):
         ttk.Frame.__init__(self, master=None)
         self.pack(fill=BOTH, expand=YES)
+
+        self.dest_frame = ttk.Frame(self)
+        self.dest_label = ttk.Label(self.dest_frame, text='target', anchor='e', font='Arial 12')
+        self.dest_text = ttk.Entry(self.dest_frame)
+        self.dest_text.focus_set()
+        self.dest_label.pack(side='left', fill=BOTH, expand=YES)
+        self.dest_text.pack(side='right', fill=BOTH, expand=YES)
+        self.dest_frame.pack(side='top', fill=BOTH, expand=YES)
+
         self.tree = ttk.Treeview(self, selectmode="extended", columns=('ttl', 'domain_name', 'ip_v4', 'rtt'))
-        self.tree.pack(fill=BOTH, expand=YES)
         # hide pseudo column
         self.tree.column('#0', stretch=NO, width=0)
         self.tree.column('ttl', stretch=YES)
@@ -20,6 +28,8 @@ class TraceView(ttk.Frame):
         self.tree.heading('ip_v4', text='IPv4')
         self.tree.column('rtt', stretch=YES)
         self.tree.heading('rtt', text='RTT')
+        self.tree.pack(fill=BOTH, expand=YES)
+
 
 
 if __name__ == '__main__':
